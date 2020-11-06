bb0b24442d3ff8b950061cd6e1bf17169cf6cd2d
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-11-06 18:41:20 -0300

    separating in tokens

--- src/main.c
+++ src/main.c
@@ -1,21 +1,37 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const char *DELIMITADOR = ",";
 
 int main(int argc, char *argv[]){
 	FILE *input, *output;
 	char *linha = NULL;
 	size_t inicio = 0;
 	ssize_t tamanho = 0;
+	char *token = NULL;
+	int count = 0;
 
 	input = fopen(argv[1], "r");
 	output = fopen("saida.txt", "w");
-	
+
 	while((tamanho = getline(&linha, &inicio, input)) != -1){
 		linha[tamanho - 1] = '\0';
-		fprintf(output, "%s\n", linha);
+		token = strtok(linha, DELIMITADOR);
+		
+		if(count == 0){
+			while(token != NULL){
+				fprintf(output, "%s", token);
+                                token = strtok(NULL, DELIMITADOR);
+			}
+			count++;
+		} else if(count == 1){
+			fprintf(output, "\n%s", linha);
+			count = 0;
+		}
 	}
-	
+
 
 	fclose(input);
 	fclose(output);
