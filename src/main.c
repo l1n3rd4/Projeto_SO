aba222728cb250d046e3d763ece7608b4dcbc613
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-11-06 13:34:02 -0300

    opening files

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char *argv[]){
+	FILE *input, *output;
 	
+	input = fopen(argv[1], "r");
+	output = fopen("saida.txt", "w");
+
+
+
+	fclose(input);
+	fclose(output);
 }
