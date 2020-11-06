a5c53233401fe2600507d25891d2f7c0ed6c3c00
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-11-06 18:07:15 -0300

    Adding I/O function

--- src/main.c
+++ src/main.c
@@ -1,14 +1,24 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
 	FILE *input, *output;
-	
+	char *linha = NULL;
+	size_t inicio = 0;
+	ssize_t tamanho = 0;
+
 	input = fopen(argv[1], "r");
 	output = fopen("saida.txt", "w");
-
-
+	
+	while((tamanho = getline(&linha, &inicio, input)) != -1){
+		linha[tamanho - 1] = '\0';
+		fprintf(output, "%s\n", linha);
+	}
+	
 
 	fclose(input);
 	fclose(output);
+
+	return (0);
 }
