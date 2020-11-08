8929c6befaec71f4f5388eebfe9fa401e23dd1b2
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-11-08 19:17:01 -0300

    refactoring

--- src/main.c
+++ src/main.c
@@ -4,37 +4,38 @@
 #include <string.h>
 
 const char *DELIMITADOR = ",";
+int readFile(FILE *input);
 
 int main(int argc, char *argv[]){
 	FILE *input, *output;
-	char *linha = NULL;
-	size_t inicio = 0;
-	ssize_t tamanho = 0;
-	char *token = NULL;
-	int count = 0;
+
 
 	input = fopen(argv[1], "r");
 	output = fopen("saida.txt", "w");
 
-	while((tamanho = getline(&linha, &inicio, input)) != -1){
-		linha[tamanho - 1] = '\0';
-		token = strtok(linha, DELIMITADOR);
-		
-		if(count == 0){
-			while(token != NULL){
-				fprintf(output, "%s", token);
-                                token = strtok(NULL, DELIMITADOR);
-			}
-			count++;
-		} else if(count == 1){
-			fprintf(output, "\n%s", linha);
-			count = 0;
-		}
-	}
-
+	readFile(input);
 
 	fclose(input);
 	fclose(output);
 
 	return (0);
+}
+
+
+int readFile(FILE *input){
+	char *linha = NULL;
+	size_t inicio = 0;
+	ssize_t tamanho = 0;
+	char *token = NULL;
+
+	while((tamanho = getline(&linha, &inicio, input)) != -1){
+		linha[tamanho - 1] = '\0';
+		token = strtok(linha, DELIMITADOR);
+
+			while(token != NULL){
+					token = strtok(NULL, DELIMITADOR);
+					printf("%s", token);
+			}
+
+	}
 }
