201cfea31539bb00187c899f7cb4368211228dc2
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-11-10 14:28:40 -0300

    CMake configuration

--- include/test.h
+++ include/test.h
@@ -1,3 +1,5 @@
 #ifndef TEST_H
 #define TEST_H
+
+#define S "LEONARDO"
 #endif
--- src/main.c
+++ src/main.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "test.h"
 
 const char *DELIMITADOR = ",";
-int readFile(FILE *input);
+//int readFile(FILE *input);
 
 int main(int argc, char *argv[]){
 	FILE *input, *output;
 
 
-	input = fopen(argv[1], "r");
-	output = fopen("saida.txt", "w");
+//	input = fopen(argv[1], "r");
+//	output = fopen("saida.txt", "w");
 
-	readFile(input);
-
-	fclose(input);
-	fclose(output);
+//	readFile(input);
+	printf("Hello  %s\n", S);
+//	fclose(input);
+//	fclose(output);
 
 	return (0);
 }
 
-
+/*
 int readFile(FILE *input, long int position){
 	char *linha = NULL;
 	size_t inicio = 0;
@@ -39,3 +40,5 @@ int readFile(FILE *input, long int position){
 
 	}
 }
+
+*/
