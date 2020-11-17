5e22c9432ea4de1d380be133e98276f87cf6fc28
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-11-17 18:19:40 -0300

    Creating empty verification function

--- src/memory.c
+++ src/memory.c
@@ -9,3 +9,12 @@ int isThere(int columnNumber, int **historic, int item){
 
     return (-1);
 }
+
+int areThereEmptySpaces(int columnNumber, int **historic, int item){
+  for(int i = 0; i < getLineSize() - 1; i++){
+    if(historic[i][columnNumber] == 0)
+      return (i);
+  }
+
+  return (-1);
+}
